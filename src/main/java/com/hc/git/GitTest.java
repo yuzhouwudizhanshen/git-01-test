a749aea9036b47fbc2c28711a9f41b4b1fbb4613
Neo <506462827@qq.com>
2022-01-25 12:17:56 +0800

    third commit

--- src/main/java/com/hc/git/GitTest.java
+++ src/main/java/com/hc/git/GitTest.java
@@ -5,6 +5,8 @@ public class GitTest {
     public static void main(String[] args) {
         System.out.println("hello git!");
         System.out.println("hello git2!");
+        System.out.println("hello git3!");
+
 
     }
 }
