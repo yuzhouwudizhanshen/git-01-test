f776094b12d943e55235cc32849e623142c91ac3
Neo <506462827@qq.com>
2022-01-25 12:15:09 +0800

    second commit

--- src/main/java/com/hc/git/GitTest.java
+++ src/main/java/com/hc/git/GitTest.java
@@ -1,7 +1,10 @@
 package com.hc.git;
 
 public class GitTest {
+
     public static void main(String[] args) {
         System.out.println("hello git!");
+        System.out.println("hello git2!");
+
     }
 }
